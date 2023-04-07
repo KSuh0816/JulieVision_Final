1efd66aad8926165c8c59c1a1a54c4ebc4fe488f
Kevin Suh <ksuh@princeton.edu>
2023-04-07 14:10:24 -0400

    Update after della

--- argonne_seq1.cmd
+++ argonne_seq1.cmd
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH -t 24:00:00
+#SBATCH -t 00:15:00
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ksuh@princeton.edu
 #SBATCH --output=argonne_sequence2.out
 #SBATCH --mem=100G
 
-module load anaconda3
-module load cudnn/cuda-10.1
-source activate tf-gpu
-python process_new_stacks_1.py
+module load anaconda3/2023.3
+module load cudatoolkit/11.6
+module load cudnn/cuda-11.x/8.2.0
+source activate KS2-gpu
+python process_new_images_1.py
